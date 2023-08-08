03f1a0ade0ece6744295dccd7c39b6008b333395
hurstacy <107961442+hurstacy@users.noreply.github.com>
2023-08-07 21:01:07 -0400

    update form

--- src/components/NewPlayerForm.jsx
+++ src/components/NewPlayerForm.jsx
@@ -10,20 +10,17 @@ export default function NewPlayerForm() {
       <h3>New Player</h3>
       <div className="field">
         <input type="text" id="name" name="name" placeholder="Name" required />
-        <small></small>
       </div>
       <div className="field">
         <input type="text" id="breed" name="breed" placeholder="Breed" required />
-        <small></small>
       </div>
       <div className="field">
         <input type="text" id="status" name="status" placeholder="Status" required />
-        <small></small>
       </div>
       <div className="field">
-        <input type='file' id="imageUrl" name="imageUrl" src="#" alt="imageUrl"/>
+        <input type='text' id="imageUrl" name="imageUrl" placeholder="Image Url" required/>
         </div>
-          <button type="submit">Submit</button>
+       <button type="submit">Submit</button>
     </form>
       </>
     );
